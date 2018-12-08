cdb5ded0bb943629c7812616eac380671d57e602
julkwel <julienrajerison5@gmail.com>
2018-12-08 12:46:28 +0300

    index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', loadChildren: 'src/app/components/login/login.module#LoginModule'},
-  {path: 'menu', loadChildren: 'src/app/components/Sekoliko/sekoliko.module#SekolikoModule'},
-  { path: '**', redirectTo: 'login' }
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', loadChildren: 'src/app/components/login/login.module#LoginModule'},
+    {path: 'menu', loadChildren: 'src/app/components/Sekoliko/sekoliko.module#SekolikoModule'},
+    {path: '**', redirectTo: 'login'}
 ];
 
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule],
-  declarations: []
+    imports: [
+        RouterModule.forRoot(routes, {useHash: true})
+    ],
+    exports: [RouterModule],
+    declarations: []
 })
 export class AppRoutingModule {
 }
