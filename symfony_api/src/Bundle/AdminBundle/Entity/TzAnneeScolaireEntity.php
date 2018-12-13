06c4d46ef6066a0a21ef9916eb2df6dceb62a928
Tahiana <tahiana.rakotonirina@esti.mg>
2018-12-13 20:08:43 +0300

    ajout entité

---
+++ symfony_api/src/Bundle/AdminBundle/Entity/TzAnneeScolaireEntity.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tux
+ * Date: 01/12/2018
+ * Time: 07:59
+ */
+
+namespace Bundle\AdminBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * TzEtablissement
+ *
+ * @ORM\Table(name="tz_annee_scolaire")
+ * @ORM\Entity(repositoryClass="Bundle\AdminBundle\Repository\TzAnneeScloaireRepository")
+ */
+class TzAnneeScolaireEntity
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Id
+     * @ORM\Column(name="num_inscription", type="integer", nullable=false)
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @JMS\Groups({"asco"})
+     * @JMS\SerializedName("AnneeScolaireID")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="date_debut", type="datetime", nullable=false)
+     * @JMS\Groups({"asco"})
+     */
+    private $dateDebut;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="date_fin", type="datetime", nullable=false)
+     * @JMS\Groups({"asco"})
+     */
+    private $dateFin;
+
+    /*
+     * @return integer
+     */
+    public function getID(){
+        return $this->id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateDebut(){
+        return $this->dateDebut;
+    }
+
+    /**
+     * @param \DateTime $date
+     */
+    public function setDateDebut($date){
+        $this->dateDebut = $date;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateFin(){
+        return $this->dateFin;
+    }
+
+    /**
+     * @param \DateTime $date
+     */
+    public function setDateFin($date){
+        $this->dateFin= $date;
+    }
+}
