162aa6b1a7cebf20a40f39ec7d969003b5564019
julkwel <julienrajerison5@gmail.com>
2018-12-14 19:40:26 +0300

    salle delete , edit

--- src/app/Utils/api/urlList.ts
+++ src/app/Utils/api/urlList.ts
@@ -5,10 +5,11 @@ const _api = environment.base_url_api + '/';
 export const urlList = {
   path_login: _api + 'users/login',
   path_list_class: _api + 'classe/list_classe',
-  path_edit_class: _api + 'class/edit',
+  path_edit_class: _api + 'class/edit/',
   path_list_salle: _api + 'salle/find',
-  path_edit_salle: _api + 'salle/edit',
+  path_edit_salle: _api + 'salle/edit/',
   path_teste_user: _api + 'users',
   path_list_etudiants: _api + 'listEtd ',
   path_list_proffesseurs: _api + 'prof/find',
+  path_delete_salle: _api + 'salle/delete/'
 };
--- src/app/components/Sekoliko/tz-salle/tz-salle.component.ts
+++ src/app/components/Sekoliko/tz-salle/tz-salle.component.ts
@@ -4,6 +4,7 @@ import {urlList} from '../../../Utils/api/urlList';
 import {ConstantHTTP} from '../../../Utils/ConstantHTTP';
 import {Subject} from "rxjs";
 import {Validators ,FormControl} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-tz-salle',
@@ -15,10 +16,15 @@ export class TzSalleComponent implements OnInit {
   listSalle = [];
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
+  id: '';
+  public description:string;
 
+  constructor(private dataService: DataService,
+              private router:Router,
+              )
+  {
 
-
-  constructor(private dataService: DataService) { }
+  }
 
   ngOnInit() {
 
@@ -30,16 +36,35 @@ export class TzSalleComponent implements OnInit {
             id: (element.id).toString(),
             nom: element.nom,
           });
-          console.log(this.listSalle);
         });
       }else {
         console.log("verifieo le function aloha papie a :D ")
       }
     });
+
   }
+
+  /**
+   * Function list , delete
+   */
   getListSalle() {
    return this.dataService.post(urlList.path_list_salle);
   }
+
+  deleteSalle(){
+    return this.dataService.post(urlList.path_delete_salle + this.id).subscribe(()=>{
+          this.router.navigate(['salle']);
+    });
+  }
+
+  editSalle(event:any){
+     this.dataService.post(urlList.path_edit_salle+event.target.id.value).subscribe(()=>{
+       this.id = event.target.salle_nom.value;
+       this.description = event.target.id.value;
+    })
+    return this;
+  }
+
 
   /**
    * Touche Pas
