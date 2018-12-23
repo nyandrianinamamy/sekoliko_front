dbf071f03510e9108925f41afd44f2e9a768487d
julkwel <julienrajerison5@gmail.com>
2018-12-23 23:45:56 +0300

    uniformalisation listing

--- src/app/components/Sekoliko/tz-profs/tz-fonction-prof/tz-fonction-prof.component.ts
+++ src/app/components/Sekoliko/tz-profs/tz-fonction-prof/tz-fonction-prof.component.ts
@@ -6,6 +6,7 @@ import urlContains = until.urlContains;
 import {urlList} from '../../../../Utils/api/urlList';
 import {ConstantHTTP} from '../../../../Utils/ConstantHTTP';
 import {Classe} from '../../../../shared/model/Classe';
+import {MatiereParam} from "../../../../../../../skbcf/src/app/shared/model/MatiereParam";
 
 @Component({
   selector: 'app-tz-fonction-prof',
@@ -16,12 +17,14 @@ export class TzFonctionProfComponent implements OnInit {
   idProf: number;
   loading: boolean;
   listClasse: Classe[];
+  listMatiere: MatiereParam;
   constructor(private currentRoute: ActivatedRoute,
               private dataService: DataService) { }
 
   ngOnInit() {
     this.idProf = this.currentRoute.snapshot.paramMap.get('id') ? +this.currentRoute.snapshot.paramMap.get('id') : null;
     this.getAllClasse();
+    this.getAllMatiere();
   }
   getAllClasse() {
     this.dataService.post(urlList.path_list_class_enfant).subscribe(response => {
@@ -29,5 +32,15 @@ export class TzFonctionProfComponent implements OnInit {
         this.listClasse = response.data;
       }
     });
+  }
+  getAllMatiere(){
+    this.loading = true
+    this.dataService.post(urlList.path_list_matiere).subscribe(response=>{
+      if (response.code === ConstantHTTP.CODE_SUCCESS){
+        this.listMatiere = response.data;
+        console.log(this.listMatiere);
+        this.loading = false
+      }
+    })
   }
 }
--- src/app/components/Sekoliko/tz-profs/tz-list-profs/tz-list-profs.component.ts
+++ src/app/components/Sekoliko/tz-profs/tz-list-profs/tz-list-profs.component.ts
@@ -1,9 +1,11 @@
-import {Component, ElementRef, HostListener, OnInit, QueryList, ViewChildren} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { DataService } from '../../../../shared/service/data.service';
 import { ConstantHTTP } from 'src/app/Utils/ConstantHTTP';
 import { urlList } from 'src/app/Utils/api/urlList';
 import {Subject} from "rxjs";
+import {MatPaginator, MatTableDataSource} from "@angular/material";
+import {Profs} from "../../../../../../../skbcf/src/app/shared/model/Profs";
 
 @Component({
   selector: 'app-tz-list-profs',
@@ -12,10 +14,19 @@ import {Subject} from "rxjs";
 })
 export class TzListProfsComponent implements OnInit {
 
+  /**
+   * Table
+   */
+  displayedColumns: string[] = ['nom', 'coefficient', 'class', 'prof', 'action'];
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  loading: boolean;
 
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
   listProff = [];
+  _listProff : Profs[];
 
   constructor(private dataService: DataService) { }
 
@@ -23,13 +34,14 @@ export class TzListProfsComponent implements OnInit {
     this.getListProffesseurs().subscribe((response: any) => {
       this.dtTrigger.next();
       if (response.code === ConstantHTTP.CODE_SUCCESS) {
+        this._listProff = response.data;
+        console.log(this._listProff);
+        this.dataSource = new MatTableDataSource<any>(this._listProff);
         response.data.forEach(element => {
           this.listProff.push({
             id: element.id,
-            nom: element.nom
-            // matiere: element.matiereId
-
-          });
+            nom: element.nom}
+            );
           console.log(this.listProff);
         });
       } else {
