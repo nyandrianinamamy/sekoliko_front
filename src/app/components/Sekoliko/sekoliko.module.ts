bfd341ff8d5b718ed289a635e15aba070d0a8f1a
Tahiana <tahiana.rakotonirina@esti.mg>
2018-12-23 21:41:00 +0300

    gestion reservation salle

--- src/app/components/Sekoliko/sekoliko.module.ts
+++ src/app/components/Sekoliko/sekoliko.module.ts
@@ -8,13 +8,13 @@ import {SekolikoComponent} from './sekoliko.component';
 import {MaterialModule} from '../../Utils/modules/Material.module';
 import {DataTablesModule} from 'angular-datatables';
 import {
-    MatButtonModule, MatButtonToggleModule,
-    MatCardModule, MatCheckboxModule, MatDatepickerModule, MatExpansionModule, MatFormFieldModule,
-    MatGridListModule,
-    MatIconModule, MatInputModule,
-    MatListModule, MatMenuModule, MatPaginatorModule, MatRadioModule, MatSelectModule,
-    MatSidenavModule, MatTableModule,
-    MatToolbarModule
+  MatButtonModule, MatButtonToggleModule,
+  MatCardModule, MatCheckboxModule, MatDatepickerModule, MatExpansionModule, MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule, MatInputModule,
+  MatListModule, MatMenuModule, MatPaginatorModule, MatRadioModule, MatSelectModule,
+  MatSidenavModule, MatTableModule,
+  MatToolbarModule
 } from '@angular/material';
 
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -34,86 +34,89 @@ import {TzAjoutEtudiantComponent} from './tz-etudiants/tz-ajout-etudiant/tz-ajou
 import {TzListProfsComponent} from './tz-profs/tz-list-profs/tz-list-profs.component';
 import {TzAjoutProfsComponent} from './tz-profs/tz-ajout-profs/tz-ajout-profs.component';
 import {TzAdmDetailsComponent} from './tz-administration/tz-adm-details/tz-adm-details.component';
-import { TzAjoutSalleComponent } from './tz-salle/tz-ajout-salle/tz-ajout-salle.component';
-import { TzAjoutAdminComponent } from './tz-administration/tz-ajout-admin/tz-ajout-admin.component';
-import { TzClasseListComponent} from './tz-classe-list/tz-classe-list.component';
-import { TzAjoutClasseComponent } from './tz-classe-list/tz-ajout-classe/tz-ajout-classe.component';
-import { TzMatiereComponent } from './tz-matiere/tz-matiere.component';
-import { TzAjoutMatiereComponent } from './tz-matiere/tz-ajout-matiere/tz-ajout-matiere.component';
-import { RadarSpinnerModule} from 'angular-epic-spinners';
-import { TzProfileComponent } from './tz-profile/tz-profile.component';
-import { TzUserComponent } from './tz-user/tz-user.component';
-import { TzInscriptionComponent } from './tz-inscription/tz-inscription.component';
-import { TzFonctionProfComponent } from './tz-profs/tz-fonction-prof/tz-fonction-prof.component';
-import { EtudiantUpdateComponent } from './tz-etudiants/etudiant-update/etudiant-update.component';
-import {DialogContentExampleDialog} from "./tz-salle/tz-salle.component";
-import { UserManageComponent } from './user-manage/user-manage.component';
+import {TzAjoutSalleComponent} from './tz-salle/tz-ajout-salle/tz-ajout-salle.component';
+import {TzAjoutAdminComponent} from './tz-administration/tz-ajout-admin/tz-ajout-admin.component';
+import {TzClasseListComponent} from './tz-classe-list/tz-classe-list.component';
+import {TzAjoutClasseComponent} from './tz-classe-list/tz-ajout-classe/tz-ajout-classe.component';
+import {TzMatiereComponent} from './tz-matiere/tz-matiere.component';
+import {TzAjoutMatiereComponent} from './tz-matiere/tz-ajout-matiere/tz-ajout-matiere.component';
+import {RadarSpinnerModule} from 'angular-epic-spinners';
+import {TzProfileComponent} from './tz-profile/tz-profile.component';
+import {TzUserComponent} from './tz-user/tz-user.component';
+import {TzInscriptionComponent} from './tz-inscription/tz-inscription.component';
+import {TzFonctionProfComponent} from './tz-profs/tz-fonction-prof/tz-fonction-prof.component';
+import {EtudiantUpdateComponent} from './tz-etudiants/etudiant-update/etudiant-update.component';
+import {UserManageComponent} from './user-manage/user-manage.component';
+import {OwlDateTimeModule, OwlNativeDateTimeModule} from 'ng-pick-datetime';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        HttpClientModule,
-        SekolikoRoutingModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        FormsModule,
-        MatListModule,
-        MatGridListModule,
-        FlexLayoutModule,
-        MatCardModule,
-        MatExpansionModule,
-        MatMenuModule,
-        MatFormFieldModule,
-        MatSelectModule,
-        MatCheckboxModule,
-        ReactiveFormsModule,
-        MatButtonToggleModule,
-        MatInputModule,
-        RadarSpinnerModule,
-        MatPaginatorModule,
-        MatRadioModule,
-        MatDatepickerModule,
-        MatTableModule,
-        MaterialModule,
-        DataTablesModule,
-        MDBBootstrapModule.forRoot(),
-    ], schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
-    declarations: [
-        SekolikoComponent,
-        TzDashboardComponent,
-        TzEtudiantsComponent,
-        TzSalleComponent,
-        FooterComponent,
-        NotFoundComponent,
-        TzProfsComponent,
-        TzPayementsComponent,
-        NavMenuComponent,
-        TzAdministrationComponent,
-        ListEtudiantsComponent,
-        TzClasseComponent,
-        TzAjoutEtudiantComponent,
-        TzAjoutProfsComponent,
-        TzListProfsComponent,
-        TzAjoutSalleComponent,
-        TzAdmDetailsComponent,
-        TzAjoutAdminComponent,
-        TzClasseListComponent,
-        TzAjoutClasseComponent,
-        TzMatiereComponent,
-        TzAjoutMatiereComponent,
-        TzUserComponent,
-        TzInscriptionComponent,
-        TzProfileComponent,
-        TzFonctionProfComponent,
-        EtudiantUpdateComponent,
-        DialogContentExampleDialog
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    SekolikoRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    FormsModule,
+    MatListModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    ReactiveFormsModule,
+    MatButtonToggleModule,
+    MatInputModule,
+    RadarSpinnerModule,
+    MatPaginatorModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatTableModule,
+    MaterialModule,
+    DataTablesModule,
+    MDBBootstrapModule.forRoot(),
+  ], schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [
+    SekolikoComponent,
+    TzDashboardComponent,
+    TzEtudiantsComponent,
+    TzSalleComponent,
+    FooterComponent,
+    NotFoundComponent,
+    TzProfsComponent,
+    TzPayementsComponent,
+    NavMenuComponent,
+    TzAdministrationComponent,
+    ListEtudiantsComponent,
+    TzClasseComponent,
+    TzAjoutEtudiantComponent,
+    TzAjoutProfsComponent,
+    TzListProfsComponent,
+    TzAjoutSalleComponent,
+    TzAdmDetailsComponent,
+    TzAjoutAdminComponent,
+    TzClasseListComponent,
+    TzAjoutClasseComponent,
+    TzMatiereComponent,
+    TzAjoutMatiereComponent,
+    TzUserComponent,
+    TzInscriptionComponent,
+    TzProfileComponent,
+    TzFonctionProfComponent,
+    EtudiantUpdateComponent
 
-    ],
+  ],
   entryComponents: [
     EtudiantUpdateComponent,
-      DialogContentExampleDialog
+    TzAjoutSalleComponent
   ],
 })
-export class SekolikoModule {}
+export class SekolikoModule {
+}
