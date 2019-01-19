40ffce4d8efbf5f59b7680059db2c310e34190ae
Ny Hasinavalona <hasinavalona.randriantsarafara@dreamslab.fr>
2019-01-19 14:22:13 +0300

    [APP] Update routes #2-refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'authentication',
+    loadChildren: './features/authentication/authentication.module#AuthenticationModule'
+  },
+  {
+    path: 'front-office',
+    loadChildren: './features/front-office/front-office.module#FrontOfficeModule'
+  },
+  {
+    path: 'login',
+    redirectTo: 'authentication',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'authentication'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
